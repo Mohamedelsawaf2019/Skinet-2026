04a53d148d1bd05560d545da6933e8e3bd024abd
Mohamed ElSawaf <melsawaf@arpuplus.com>
2026-09-15 03:31:02 +0300

    Section 3

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -21,15 +21,37 @@ namespace Infrastructure.Data
             context.products.Remove(product);
         }
 
+        public async Task<IReadOnlyList<string>> GetBrandsAsync()
+        {
+            return await context.products.Select(x=> x.Brand).Distinct().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<string>> GetTypesAsync()
+        {
+            return await context.products.Select(x => x.Type).Distinct().ToListAsync();
+        }
+
         public async Task<Product?> GetProductByIdAsync(int id)
         {
             return await context.products.FindAsync(id);
         }
 
-        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        public async Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? type, string? sort)
         {
-            return await context.products.ToListAsync();
+            var query = context.products.AsQueryable();
+            if(!string.IsNullOrWhiteSpace(brand))
+                query = query.Where(x=> x.Brand ==  brand);
+            if (!string.IsNullOrWhiteSpace(type))
+                query = query.Where(x => x.Type == type);
+            query = sort switch
+            {
+                "priceAsc" => query = query.OrderBy(x => x.Price),
+                "priceDesc" => query = query.OrderByDescending(x => x.Price),
+                _ => query = query.OrderBy(x => x.Name)
+            };
+            return await query.ToListAsync();
         }
+
 
         public bool ProductExists(int id)
         {
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System.Text.Json;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context)
+        {
+            if (!context.products.Any())
+            {
+                var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+                var products = JsonSerializer.Deserialize<List<Product>>(productsData, new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+                if (products == null) return;
+                await context.products.AddRangeAsync(products);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
